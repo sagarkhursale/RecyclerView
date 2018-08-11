82a8281073987bbb5b7bb778affc3bc1df71e64e
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-12 00:45:33 +0530

    activity and list item ui.

--- app/src/main/java/com/sagar/recyclerview/MainActivity.java
+++ app/src/main/java/com/sagar/recyclerview/MainActivity.java
@@ -2,13 +2,17 @@ package com.sagar.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNumbersList =  findViewById(R.id.rv_numbers);
 
 
     }
